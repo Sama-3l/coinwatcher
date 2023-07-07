cba4d33008f170ab97a0574f607953f9ee665106
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-07-07 12:58:13 +0530

    working

--- lib/alogrithms/widgetDecider.dart
+++ lib/alogrithms/widgetDecider.dart
@@ -1,5 +1,24 @@
 //Add functions that decide what widget will be there in the UI
 
-class WidgetDecider{
-  
+import 'package:coinwatcher/data/repositories/allExpenses.dart';
+import 'package:coinwatcher/data/repositories/recentExpenses.dart';
+import 'package:flutter/material.dart';
+
+class WidgetDecider {
+  Widget getSpendingsWidgets(
+      AllExpenses allExpenses, RecentExpenses recentExpenses, int index) {
+    List<Widget> columnChildren = [];
+
+    if (index < recentExpenses.recentExpenses.length) {
+      if (index == 0) {
+        columnChildren.add(Text("Recent Spendings"));
+      }
+    }
+
+    if (columnChildren.length == 1) {
+      return columnChildren[0];
+    } else {
+      return Column(children: columnChildren);
+    }
+  }
 }
--- lib/presentation/screens/spendings.dart
+++ lib/presentation/screens/spendings.dart
@@ -1,4 +1,7 @@
+import 'package:coinwatcher/alogrithms/widgetDecider.dart';
 import 'package:coinwatcher/constants/themes.dart';
+import 'package:coinwatcher/data/repositories/allExpenses.dart';
+import 'package:coinwatcher/data/repositories/recentExpenses.dart';
 import 'package:flutter/material.dart';
 
 class Spendings extends StatefulWidget {
@@ -11,11 +14,16 @@ class Spendings extends StatefulWidget {
 }
 
 class _SpendingsState extends State<Spendings> {
+
+  AllExpenses allExpenses = AllExpenses();
+  RecentExpenses recentExpenses = RecentExpenses();
+  WidgetDecider wd = WidgetDecider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: widget.theme.mainBackground,
-      body: Container(),
+      body: ListView.builder(itemBuilder: (context, index) {}),
     );
   }
 }
